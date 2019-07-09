cc27f60a92f236dddf0c214d04f04ebdb872e21e
f-myself <freaking.yourself@gmail.com>
2019-07-09 17:11:51 +0300

    template

---
+++ src/components/Auth/Auth.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        Hello
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      none: ''
+    }
+  },
+  methods: {
+      test: function() {
+        var formData  = new FormData();
+        formData.append('login', 'user');
+        formData.append('password', 'password');
+
+        fetch('http://booker.loc/Server/app/api/auth/', 
+        {method: "PUT",  body: formData}).then((response) => {
+            console.log(response);
+        });
+      }
+  },
+  created: function() {
+      this.test();
+  }
+}
+</script>
